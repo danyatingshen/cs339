b6085a963be4dc11bd3cd819517aacd4925e6bdc
echoochoo <39955757+echoochoo@users.noreply.github.com>
2020-02-17 22:11:16 -0500

    debugged and praying it works

--- hw1/knn.py
+++ hw1/knn.py
@@ -7,7 +7,9 @@ import operator
 import numpy as np
 import pandas as pd
 from math import exp
+import copy 
 
+## 1ai) function that computed euclidean distance
 ## find the distance between two vector points
 ## assuming the input is (class, input1, input2)
 def euclid_distance(vectorA, vectorB):
@@ -18,6 +20,7 @@ def euclid_distance(vectorA, vectorB):
     distance = math.sqrt(dist)
     return distance
 
+## 1aii) computer euclidean distance between particular feature vector and each data point in training set
 ## find the distance between the training set and a given vector
 ## assuming the given vector = [class, input1, input2]
 ## assuming the training set = ([class, input1_1, input1_2], [class, input2_1, 2_2]...)
@@ -27,38 +30,47 @@ def distALL(given, train):
         all_dist.append(euclid_distance(given, train[i]))
     return all_dist
 
+## 1aiii) find the indices by first adding the distance and then finding k nearest
 ## want to add the distance to the vector values so you know which vector has which distance to given feature vector
 ## expected ourput will be [class, input1, input2, distance]
 def add(train, dist_all):
-    for i in range(len(train)):
-        train[i].append(dist_all[i])
-    return train
+    temp_added = copy.deepcopy(train) #to avoid keep indexing the distance again and again.
+    for i in range(len(train)): 
+        temp_added[i].append(dist_all[i])
+    return temp_added
 
 ## find the k nearest neighbors
 def k_neigh(data, k):
-    data.sort(key = lambda x: x[3]) ## sort by the third index (distance)
+    #### We sort by the distance, which is at the end of the list
+    data.sort(key = lambda x: x[-1]) ## sort by the last index (distance)
     return data[0:k]
 
+## 1aiv) find maojrity class of the k nearest neighbor
 ## find what the majority class is in the k nearest neighbors
 def majority_class(k_neigh):
-    most_class = []
+    most_class = {}
     for i in range(len(k_neigh)):
-        most_class.append(k_neigh[i][0])
-    #np.bincount(most_class).argmax()
-    return most_frequent(most_class)
+        label = k_neigh[i][0]
+        if label in most_class:
+            most_class[label] = most_class[label]
+        else:
+            most_class[label] = 1
+    # now with the LabelVotes, we sort and pick the label type with greatest number of matches
+    return max(most_class.items(), key = operator.itemgetter(1))[0]
 
 def most_frequent(List): 
     return max(set(List), key = List.count)
 
+## 1av) find the majority class for all the points in dataset, return y and y_hat
 ## find the majority class of k nearest neighbors for dataset
 ## if there is a tie between  indecies that appear in equal frequency, then choses the lowest index
 def knn(train, test, k):
     y_hat = []
     y = []
-    for i in range(len(test)): ## for every row in test
-        y.append(test[i][0])
+    for i in range(len(test)): ## for every test row
+        y.append(test[i][0]) ## The first element. 
         distance = distALL(test[i], train) ## create matrix of distances for the first test value
-        temp = add(train, distance) ## create temp dataset that has the distncaes in it
+        temp = add(train, distance) ## create temp dataset that has the distances in it
         k_values = k_neigh(temp, k) ## find the k nearest neighbors
         y_hat.append(majority_class(k_values)) ## append the most common value
     return y, y_hat
@@ -119,23 +131,18 @@ def cross_validation (J, trainning, knn, seed, k, istraining):
     master_fold_list = list(generator)
     performance_fold = list()
     performance_generalization = list()
-    #print(master_fold_list)
     # option 1: returns the performance of the classifier for each fold.
     # option 2: 1,(234) -> 2,(134)..., return the generalization error
     for v_index in range(len(master_fold_list)) :
         test = master_fold_list[v_index]
-        #print("test",test)
         temp = master_fold_list[:v_index]+master_fold_list[v_index+1:]
         train = helper_depack(temp)
-        #print("train",train)
-        y = helper_find_y(test)
-        #print("y",y)
+        y = helper_find_y(test)     # the labels
         score = evaluate_misclassify (knn,test, y, misclassify_rate, k, train)
         performance_fold.append(score)    
         if (istraining == True) :
             score_2 = evaluate_misclassify (knn,test, y, misclassify_rate, k, test)
             performance_generalization.append(score_2)
-    #print(performance_fold,performance_generalization)
     return performance_fold, performance_generalization
 
 def helper_find_y (test) :
@@ -150,6 +157,7 @@ def helper_depack (train) :
         for j in i :
             result.append(j)
     return result
+
 #----------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------
 # input: 
@@ -173,29 +181,41 @@ def mean_performance (performance_fold,performance_generalization):
         result.append(avg_2)
     return result
     
-#----------------------------------------------------------------------------------
+
 def define_data ():
-    df_1 = pd.read_csv("/Users/amanda/Desktop/oberlin/cs339/hw1/S1test.csv", header = None)
-    df_2 = pd.read_csv("/Users/amanda/Desktop/oberlin/cs339/hw1/S1train.csv", header = None)
+    df_1 = pd.read_csv("S1test.csv", header = None)
+    df_2 = pd.read_csv("S1train.csv", header = None)
     return df_1, df_2
 
 def read_data(df):
     #df = pd.read_csv("../S1test.csv", header = None)
     data = []
     dim = np.shape(df)
+    columns = df.columns
     for i in range(dim[0]): ## for every row
         temp = []
         for j in range(dim[1]): ## append every column value
-            temp.append(df[j][i]) ## array of (class, input1, input2)
+            temp.append(df[columns[j]][i]) ## array of (class, input1, input2)
         data.append(temp)
     return data
-#----------------------------------------------------------------------------------
+
+def read(df):
+    data = []
+    dim = np.shape(df)
+    for (indx, row) in df.iterrows():
+        temp = row.to_list()
+        data.append(temp)
+    return data
+
+
+
 def main () :
     J = 10
     k = 3
     seed = 123
     test, train = define_data()
-    test = read_data(test)
+    test_1 = read_data(test)
+    another_test = read(test)
     train = read_data(train)
     
     fold, gen = cross_validation (J, train, knn, seed, k, True)
@@ -206,6 +226,4 @@ def main () :
         print(item)
 
 
-
-if __name__ == '__main__':
-    main()
+main()
