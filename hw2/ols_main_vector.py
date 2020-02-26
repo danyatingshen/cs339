59b5f2de9b40c535deceaa9159297ae4296d2dbf
echoochoo <39955757+echoochoo@users.noreply.github.com>
2020-02-26 01:31:34 -0500

    Add files via upload

--- hw2/ols_main_vector.py
+++ hw2/ols_main_vector.py
@@ -12,6 +12,9 @@ from scipy.spatial import distance
 from matplotlib import pyplot as plt
 from numpy import ones
 
+## The global global variable that we will divide by at the end and then multiply for standardization
+global_max_X = 1
+
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 # input : 
@@ -23,7 +26,10 @@ def ols_coefficent_prediction_lamda (X,t,lamb):
     lamb_I = np.multiply(lamb,I)
     X_transpose_X_lamb_I = np.add(X_transpose_X,lamb_I)
     X_transpose_t = np.dot(X_transpose,t)
-    w = np.linalg.solve(X_transpose_X_lamb_I,X_transpose_t)
+    try:
+        w = np.linalg.solve(X_transpose_X_lamb_I,X_transpose_t)
+    except:
+        w= np.linalg.lstsq(X_transpose_X_lamb_I,X_transpose_t, rcond=None)[0]
     return w
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
@@ -51,16 +57,39 @@ def generate_predition_vector (x,w):
     x_index = 0
     for index in range(0,len(x)) :
         f = 0
-        current = x[index]
+        current = x[index] 
         for power in range(0,len(w)):
             f = f + w[power] * current**power
         y.append(f)
     y = np.array(y)
     return y
+'''
+def generate_predition_vector (x,w):
+    y = list()
+    x_index = 0
+    ## Before proceeding, we need to make sure if 
+    # x array is not array of array
+    #if ((type(x[0]) is np.ndarray) == True):
+        # if in this for-loop, it means test variable is an array of an array
+        #x = x[0]  
+    for index in range(0,len(x)) :
+        f = 0
+        #current = x[index][1]
+        #x[index][1] = x[index][1]*global_max_X
+        
+
+        for power in range(0,len(w)):
+            # we also need to multiply back our global max x
+            current = x[index][power]*global_max_X
+            f = f + w[power] * current**power
+        y.append(f)
+    y = np.array(y)
+    return y
+'''
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 def define_data ():
-    df = pd.read_csv("synthdata2016.csv", header = None)
+    df = pd.read_csv("womens100.csv", header = None)
     return df
 
 def read(df):
